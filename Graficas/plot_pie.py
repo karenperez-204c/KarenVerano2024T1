97b20eb050000a2b9de05af5fcf432b7386a064a
Karen Perez Ortiz <230110326@itsoeh.edu.mx>
2024-06-27 09:31:44 -0600

    Ejercicio 8: inciso a

--- Graficas/plot_pie.py
+++ Graficas/plot_pie.py
@@ -9,5 +9,5 @@ def plot_pie(datos, marcas_texto):
             pctdistance=0.8, 
             colors=["#37CA64", "#FF99F3", "#37C5CA", "#E74E50", "#FC8C29", "#DEE74E"], 
             labels=marcas_texto)
-    plt.title("Distribución de Sabores", fontsize=20)
+    plt.title("Grafica de Pastel", fontsize=20)
     plt.show()
